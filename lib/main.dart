853577b2e47b4fac4042bd08b16aaaadcdacf6cf
ahmednazeem <47294582+ahmednazeem@users.noreply.github.com>
2024-01-04 20:33:33 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+main() {
+  return runApp(
+    Myapp(),
+  );
+}
+
+class Myapp extends StatefulWidget {
+  @override
+  _MyappState createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/bg3.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
